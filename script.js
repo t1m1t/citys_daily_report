95228bdeea30863dc266aa9cd0d7e68d98c5ee6c
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-06-29 12:03:22 -0700

    weather not working

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+var lat1 = 33.6846;
+var lng = -117.8265;
+var data = null;
 $(document).ready(function () {
     // $("#start_over").click(start_over);
     initialize();
-    $.ajax(displayWeather);
     $.ajax(chuckNorris);
+    $.ajax(displayWeather);
     // $('#address').click(zoomToArea);
-
-
+    $("#address_button").click(function(){
+        console.log("clicked");
+        codeAddress();
+    });
+    $("#weather_button").click(function() {
+        console.log("weather button clicked");
+        $.ajax(displayWeather);
+    });
 });
 var geocoder;
 var map;
@@ -28,74 +37,18 @@ function codeAddress() {
                 map: map,
                 position: results[0].geometry.location
             });
+            console.log(results[0].geometry.location.lat());
+            console.log(results[0].geometry.location.lng());
+            lat1 = results[0].geometry.location.lat();
+            lng = results[0].geometry.location.lng();
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
     });
 }
-
-// var map;
-// function createMap() {
-//     // Creates and centers map
-//     var location = {lat: -25.363, lng: 131.044};
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         zoom: 4,
-//         center: location
-//     });
-//
-// }
-//
-// google.maps.event.addDomListener(window, "load", initMap);
-
-// var x = {};
-// var navigatorFunction = function() {
-//     if(navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(function(position) {
-//             x.latitude = position.coords.latitude;
-//             x.longitude = position.coords.longitude;
-//             console.log(x);
-//         });
-//     } else {
-//         console.error("navigator.geolocation does not exist.");
-//         return false;
-//     }
-// };
-
-
-var geocoder;
-var map;
-
-function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(33.6846,-117.8265);
-    var mapOptions = {
-        zoom: 8,
-        center: latlng
-    };
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
-
-function codeAddress() {
-    var address = document.getElementById('address').value;
-    geocoder.geocode( {'address': address}, function(results, status) {
-        if (status == 'OK') {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
-
-
-var long = 33.740000;
-var lat = -117.730000;
 
 var displayWeather ={
-    url: 'http://api.wunderground.com/api/1348f5771c1ee038/conditions/forecast/q/'+long+','+lat+'.json ',
+    url: 'http://api.wunderground.com/api/1348f5771c1ee038/conditions/forecast/q/'+lng+','+lat1+'.json ',
     success: receiveDataFromServer,
     error: errorFromServer,
     dataType: 'json',
@@ -107,7 +60,7 @@ function errorFromServer(){
 }
 
 function receiveDataFromServer(data){
-    console.log('function was called');
+    console.log('display weather function was called');
     console.log(data);
     console.log(data.current_observation.display_location.full);
     console.log(data.current_observation.icon_url);
@@ -137,12 +90,4 @@ function receiveChuckData(chuck){
     console.log(chuck.value);
     $('#displayDiv').text(chuck.value);
 }
-
-var displayGooglePlaces ={
-    url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ40CRaA7d3IAROZpKYHW1eqc&key=AIzaSyBJBVHYo1-ACgQdeMcJsl3obfU49O7UAHI',
-    success: receiveDataFromServer,
-    error: errorFromServer,
-    dataType: 'json',
-    method: 'get'
-};
 
